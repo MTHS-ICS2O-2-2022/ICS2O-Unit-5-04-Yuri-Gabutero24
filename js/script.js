a989dc95cd8731b7074a090e7a541e99a67a790d
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-05-31 00:11:34 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yuri G
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculate() {
+  const age = document.getElementById("user-age").value
+  document.getElementById("user-age").innerHTML = age
+  const day = document.getElementById("day").value
+  document.getElementById("day").innerHTML = day
+
+  if (day === "tuesday" || day === "thursday" || (age >= 12 && age <= 21)) {
+    document.getElementById("determine").innerHTML =
+      "You can get into the musuem."
+  } else {
+    document.getElementById("determine").innerHTML =
+      "Sorry you can't get into the musuem"
+  }
+}
